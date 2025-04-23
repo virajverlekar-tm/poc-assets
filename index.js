999e3f8453600a8e657d84094cb0091501feba2d
Viraj Verlekar <viraj.v@turtlemint.com>
2025-04-23 11:25:34 +0530

    Initial

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+export default class PocAssets extends React.PureComponent {
+  render() {
+    return (
+      <View>
+      	<Text>
+      		PocAssets
+      	</Text>
+      </View>
+    );
+  }
+}
